e3df1308c8dab403e21d94a574ec7c8750df69ef
Bruno Miguel <migueldireito@gmail.com>
2021-08-02 12:19:11 +0100

    heudiconv necsus

--- convert-bids-admin/LOCAL/heudiconv_conv_necsus.sh
+++ convert-bids-admin/LOCAL/heudiconv_conv_necsus.sh
@@ -3,11 +3,21 @@
 ROOT_OUPUT='/home/brunomiguel/Documents/data/BIDS_necsus'
 ROOT_CODE='/home/brunomiguel/Documents/GitHub/NECSUS/convert-bids-admin/LOCAL/'
 
-heudiconv -d /media/brunomiguel/TOSHIBA\ EXT/DATA_Carlos_PhD_Final/{subject}/*/DATA/*.dcm \
---anon-cmd $ROOT_CODE//rename_script_vswp.py \
--s BrunoDireito \
---ses 001 \
--f $ROOT_CODE/heuristic_vswp.py \
+heudiconv -d /media/brunomiguel/TOSHIBA\ EXT/NECSUS/{subject}/*.dcm \
+--anon-cmd $ROOT_CODE//rename_script_necsus.py \
+-s SUBNECSUSUC001 \
+--ses 01 \
+-f $ROOT_CODE/heuristic_necsus.py \
+-c dcm2niix -b \
+-o $ROOT_OUPUT \
+--overwrite
+
+
+heudiconv -d /media/brunomiguel/TOSHIBA\ EXT/NECSUS/{subject}/*.dcm \
+--anon-cmd $ROOT_CODE//rename_script_necsus.py \
+-s SUBNECSUSUC002_1 \
+--ses 01 \
+-f $ROOT_CODE/heuristic_necsus.py \
 -c dcm2niix -b \
 -o $ROOT_OUPUT \
 --overwrite
