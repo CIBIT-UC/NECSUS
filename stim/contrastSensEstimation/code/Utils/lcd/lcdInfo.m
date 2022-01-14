b78dc1574493e12a8fd7a3fe5018fd7f02bd51f7
Bruno Miguel <migueldireito@gmail.com>
2022-01-14 14:20:31 +0000

    UM with glare tests

--- stim/contrastSensEstimation/code/Utils/lcd/lcdInfo.m
+++ stim/contrastSensEstimation/code/Utils/lcd/lcdInfo.m
@@ -8,9 +8,15 @@ lcd=struct();
 
 lcd.pathToGreyData=pathToGreyData;
 
-lcd.screenHeight=29.6;% 25.9; % Screen height (cm) nb. 34.7.
-lcd.screenWidth=52.7;% 54.6; % Screen width (cm).
+% Debug office.
+lcd.screenHeight=25.9; % Screen height (cm) 
+lcd.screenWidth=54.6; % Screen width (cm).
 lcd.viewingDistance=VIEWINGDISTANCE; %150; % Viewing Distance (cm).
+
+% % % At home.
+% % lcd.screenHeight=29.6; % Screen height (cm) 
+% % lcd.screenWidth=52.7; % Screen width (cm).
+% % lcd.viewingDistance=VIEWINGDISTANCE; %150; % Viewing Distance (cm).
 
 lcd.bitres=8; % Bits.
 
--- stim/contrastSensEstimation/code/Utils/stimuli/designFixationCross.m
+++ stim/contrastSensEstimation/code/Utils/stimuli/designFixationCross.m
@@ -3,9 +3,9 @@ function [fCross] = designFixationCross( )
 %   Detailed explanation goes here
    
     % set of size of the arms of our fixation cross
-    fCross.fixCrossDimPix=4;%20;
+    fCross.fixCrossDimPix=20;
     % set the line width for our fixation cross
-    fCross.lineWidthPix=4;
+    fCross.lineWidthPix=2;
         
     % set the coordinates for fixation cross in the center of the screen
     fCross.xCrossCoords=[-fCross.fixCrossDimPix fCross.fixCrossDimPix 0 0];
